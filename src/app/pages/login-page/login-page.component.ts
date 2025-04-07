08df33b1af910ffc5af767b3c4413a005edc4b93
Ma1n <kskiller2020@gmail.com>
2025-04-07 16:55:38 +0300

    access or reject to enter on main page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -10,7 +11,7 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class LoginPageComponent {
   authService = inject(AuthService)
-
+  router = inject(Router)
  form = new FormGroup({
   username:new FormControl(null, Validators.required),
   password:new FormControl(null, Validators.required)
@@ -23,7 +24,7 @@ export class LoginPageComponent {
     //@ts-ignore
     this.authService.login(this.form.value)
     .subscribe(res =>{
-      console.log(res)
+     this.router.navigate([''])
     })
   }
  }
